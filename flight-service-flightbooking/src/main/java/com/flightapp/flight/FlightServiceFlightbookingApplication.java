a67646739732ed02ec1bd54c71c830610a3e357a
sravya <podilasravya7052@gmail.com>
2025-12-06 00:11:31 +0530

    Update application main classes

--- flight-service-flightbooking/src/main/java/com/flightapp/flight/FlightServiceFlightbookingApplication.java
+++ flight-service-flightbooking/src/main/java/com/flightapp/flight/FlightServiceFlightbookingApplication.java
@@ -1,8 +1,11 @@
 package com.flightapp.flight;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,6 +13,17 @@ public class FlightServiceFlightbookingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlightServiceFlightbookingApplication.class, args);
+		
 	}
+	@Bean
+    CommandLineRunner printConfig(Environment env) {
+        return args -> {
+            System.out.println("======= CONFIG VALUES LOADED =======");
+            System.out.println("Application: " + env.getProperty("spring.application.name"));
+            System.out.println("Port: " + env.getProperty("server.port"));
+            System.out.println("Mongo URI: " + env.getProperty("spring.data.mongodb.uri"));
+            System.out.println("====================================");
+        };
+    }
 
 }

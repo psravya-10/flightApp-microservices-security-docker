e550a755a00edd7b5b70b4fcfdbfff4ff828f10f
sravya <podilasravya7052@gmail.com>
2025-12-08 18:10:23 +0530

    added models

---
+++ auth-service-flightbooking/src/main/java/com/flightapp/auth/model/User.java
@@ -0,0 +1,30 @@
+package com.flightapp.auth.model;
+
+import java.util.Set;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document(collection = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    private String id;
+
+    private String username;
+
+    private String email;
+
+    private String password;
+
+    private Set<String> roles; 
+}

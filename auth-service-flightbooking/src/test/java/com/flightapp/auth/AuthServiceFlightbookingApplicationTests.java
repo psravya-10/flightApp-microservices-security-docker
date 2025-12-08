e5fde8030d995296ed9e4ace5c9ac40b2cf20264
sravya <podilasravya7052@gmail.com>
2025-12-08 18:19:04 +0530

    added gitignore

---
+++ auth-service-flightbooking/src/test/java/com/flightapp/auth/AuthServiceFlightbookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flightapp.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceFlightbookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

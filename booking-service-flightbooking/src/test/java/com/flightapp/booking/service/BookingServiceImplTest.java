90db8e7c44402aae0402530901c6ef996f0ed66f
sravya <podilasravya7052@gmail.com>
2025-12-21 18:13:38 +0530

    modified

--- booking-service-flightbooking/src/test/java/com/flightapp/booking/service/BookingServiceImplTest.java
+++ booking-service-flightbooking/src/test/java/com/flightapp/booking/service/BookingServiceImplTest.java
@@ -8,9 +8,11 @@ import com.flightapp.booking.repository.BookingRepository;
 import com.flightapp.booking.feign.FlightClient;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -20,101 +22,105 @@ import java.util.Optional;
 import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
+
 class BookingServiceImplTest {
+	 @Mock
+	    private BookingRepository bookingRepo;
 
-    @Mock
-    private BookingRepository bookingRepo;
+	    @Mock
+	    private FlightClient flightClient;
 
-    @Mock
-    private FlightClient flightClient;
+	    @Mock
+	    private BookingEventPublisher bookingEventPublisher;
 
-    @InjectMocks
-    private BookingServiceImpl bookingService;  
+	    @InjectMocks
+	    private BookingServiceImpl bookingService;
 
-    private Booking booking;
-    private FlightSearchResponse flight;
+	    private Booking booking;
+	    private FlightSearchResponse flight;
 
-    @BeforeEach
-    void init() {
-        MockitoAnnotations.openMocks(this);
+	    @BeforeEach
+	    void init() {
+	        booking = new Booking();
+	        booking.setPnr("PNR001");
+	        booking.setCustomerName("Sravya");
+	        booking.setEmail("sravya@gmail.com");
+	        booking.setJourneyDate(LocalDate.of(2025, 12, 10));
+	        booking.setNumberOfSeats(2);
+	        booking.setSeatNumbers("12A,12B");
+	        booking.setMealPreference("VEG");
+	        booking.setFlightId("F001");
+	        booking.setBookingTime(LocalDateTime.now());
+	        booking.setPassengers(List.of(
+	                new Passenger("Sravya", "F", 21),
+	                new Passenger("Anu", "F", 22)
+	        ));
 
-        booking = new Booking();
-        booking.setPnr("PNR001");
-        booking.setCustomerName("Sravya");
-        booking.setEmail("sravya@gmail.com");
-        booking.setJourneyDate(LocalDate.of(2025, 12, 10));
-        booking.setNumberOfSeats(2);
-        booking.setSeatNumbers("12A,12B");
-        booking.setMealPreference("VEG");
-        booking.setFlightId("F001");
-        booking.setBookingTime(LocalDateTime.now());
-        booking.setPassengers(List.of(
-                new Passenger("Sravya", "F", 21),
-                new Passenger("Anu", "F", 22)
-        ));
+	        flight = new FlightSearchResponse();
+	        flight.setFlightId("F001");
+	        flight.setAirlineName("Indigo");
+	        flight.setFromPlace("Hyderabad");
+	        flight.setToPlace("Bangalore");
+	        flight.setTripType("ONE-WAY");
+	        flight.setDepartureTime(LocalDateTime.of(2025, 12, 10, 10, 0));
+	        flight.setArrivalTime(LocalDateTime.of(2025, 12, 10, 12, 0));
+	        flight.setAvailableSeats(120);
+	    }
 
-        flight = new FlightSearchResponse();
-        flight.setFlightId("F001");
-        flight.setAirlineName("Indigo");
-        flight.setFromPlace("Hyderabad");
-        flight.setToPlace("Bangalore");
-        flight.setTripType("ONE-WAY");
-        flight.setDepartureTime(LocalDateTime.of(2025, 12, 10, 10, 0));
-        flight.setArrivalTime(LocalDateTime.of(2025, 12, 10, 12, 0));
-        flight.setAvailableSeats(120);
-    }
+	    @Test
+	    void testGetTicketByPnr() {
+	        when(bookingRepo.findByPnr("PNR001")).thenReturn(Optional.of(booking));
+	        when(flightClient.getFlightById("F001")).thenReturn(flight);
 
-   
-    @Test
-    void testGetTicketByPnr() {
-        when(bookingRepo.findByPnr("PNR001")).thenReturn(Optional.of(booking));
-        when(flightClient.getFlightById("F001")).thenReturn(flight);
+	        TicketResponse response = bookingService.getTicketByPnr("PNR001");
 
-        TicketResponse response = bookingService.getTicketByPnr("PNR001");
+	        assertEquals("PNR001", response.getPnr());
+	        assertEquals("Indigo", response.getAirlineName());
 
-        assertEquals("PNR001", response.getPnr());
-        assertEquals("Indigo", response.getAirlineName());
-        verify(bookingRepo, times(1)).findByPnr("PNR001");
-        verify(flightClient, times(1)).getFlightById("F001");
-    }
+	        verify(bookingRepo).findByPnr("PNR001");
+	        verify(flightClient).getFlightById("F001");
+	    }
 
-    
-    @Test
-    void testBookTicketSuccess() {
-        BookingRequest req = new BookingRequest();
-        req.setCustomerName("Sravya");
-        req.setEmail("sravya@gmail.com");
-        req.setJourneyDate(LocalDate.of(2025, 12, 10));
-        req.setNumberOfSeats(2);
-        req.setSeatNumbers("12A,12B");
-        req.setMealPreference("VEG");
-        req.setPassengers(List.of(
-                new PassengerDto("Sravya", "F", 21),
-                new PassengerDto("Anu", "F", 22)
-        ));
+	    @Test
+	    void testBookTicketSuccess() {
+	        BookingRequest req = new BookingRequest();
+	        req.setCustomerName("Sravya");
+	        req.setEmail("sravya@gmail.com");
+	        req.setJourneyDate(LocalDate.of(2025, 12, 10));
+	        req.setNumberOfSeats(2);
+	        req.setSeatNumbers("12A,12B");
+	        req.setMealPreference("VEG");
+	        req.setPassengers(List.of(
+	                new PassengerDto("Sravya", "F", 21),
+	                new PassengerDto("Anu", "F", 22)
+	        ));
 
-        when(flightClient.getFlightById("F001")).thenReturn(flight);
-        when(bookingRepo.save(any())).thenReturn(booking);
+	        when(flightClient.getFlightById("F001")).thenReturn(flight);
+	        when(bookingRepo.save(any())).thenReturn(booking);
 
-        TicketResponse response = bookingService.bookTicket("F001", req);
+	        TicketResponse response = bookingService.bookTicket("F001", req);
 
-        assertEquals("Sravya", response.getCustomerName());
-        assertEquals("Indigo", response.getAirlineName());
-        verify(flightClient, times(1)).reserveSeats(eq("F001"), any());
-        verify(bookingRepo, times(1)).save(any());
-    }
+	        assertEquals("Sravya", response.getCustomerName());
+	        assertEquals("Indigo", response.getAirlineName());
 
-    
-    @Test
-    void testBookTicketWrongDate() {
-        BookingRequest req = new BookingRequest();
-        req.setJourneyDate(LocalDate.of(2025, 12, 11)); 
-        req.setNumberOfSeats(1);
-        req.setPassengers(List.of(new PassengerDto("Sravya", "F", 21)));
+	        verify(flightClient).reserveSeats(eq("F001"), any());
+	        verify(bookingRepo).save(any());
+	        verify(bookingEventPublisher).publishBookingConfirmed(any(Booking.class));
+	    }
 
-        when(flightClient.getFlightById("F001")).thenReturn(flight);
+	    @Test
+	    void testBookTicketWrongDate() {
+	        BookingRequest req = new BookingRequest();
+	        req.setJourneyDate(LocalDate.of(2025, 12, 11));
+	        req.setNumberOfSeats(1);
+	        req.setPassengers(List.of(new PassengerDto("Sravya", "F", 21)));
 
-        assertThrows(BadRequestException.class,
-                () -> bookingService.bookTicket("F001", req));
-    }
+	        when(flightClient.getFlightById("F001")).thenReturn(flight);
+
+	        assertThrows(BadRequestException.class,
+	                () -> bookingService.bookTicket("F001", req));
+	    }
 }
+
+
